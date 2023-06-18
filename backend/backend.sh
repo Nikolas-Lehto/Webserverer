db6939acc7d8e1ae3c1214f7c82812a1d5e3e0ff
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-06-18 21:06:01 +0300

    Fixed one last error (I think) in backend.sh

--- backend/backend.sh
+++ backend/backend.sh
@@ -21,12 +21,12 @@ response='HTTP/1.1 200 OK\n\n%s' "$(cat ../frontend/index.html)"
 touch ../logs/server.log
 
 while true; do 
-    if [$verbose -eq true]; then
+    if [$verbose]; then
         # If the client entered the -v flag, print the output and save it to the log file
-        $(response) > nc -lvq 1 -p ${port:-8888} | tee -a "../logs/server.log"
+        $response | nc -lvq 1 -p ${port:-8888} | tee -a "../logs/server.log"
     else
         # If the client didn't enter the -v flag, save the output without printing it and notify about the connection 
-        $(response) > nc -lvq 1 -p ${port:-8888} >> "../logs/server.log"
+        $response | nc -lvq 1 -p ${port:-8888} >> "../logs/server.log"
         echo "Connection received."
     fi
 done
