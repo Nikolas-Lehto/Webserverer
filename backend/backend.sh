f81a37c834d20a7f5ba86ed6761e5a5f6b68d14e
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-06-18 21:23:10 +0300

    Updated .gitignore acordingly

--- backend/backend.sh
+++ backend/backend.sh
@@ -18,15 +18,14 @@ while getopts ":v:p:" option; do
 done
 
 response='HTTP/1.1 200 OK\n\n%s' "$(cat ../frontend/index.html)"
-cat > ../logs/server.log
-
+touch server.log
 while true; do 
     if [$verbose]; then
         # If the client entered the -v flag, print the output and save it to the log file
-        $response | nc -lvq 1 -p ${port:-8888} | tee -a "../logs/server.log"
+        $response | nc -lvq 1 -p ${port:-8888} | tee -a "server.log"
     else
         # If the client didn't enter the -v flag, save the output without printing it and notify about the connection 
-        $response | nc -lvq 1 -p ${port:-8888} >> "../logs/server.log"
+        $response | nc -lvq 1 -p ${port:-8888} >> "server.log"
         echo "Connection received."
     fi
 done
