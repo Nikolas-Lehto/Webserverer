7e17c146d996a88c42a81b37e0a08b4892a42805
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-06-19 13:21:57 +0300

    Added "help" option to backend.sh

--- backend.sh
+++ backend.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+usage(){
+    printf " \
+    Usage: ./backend.sh [OPTION]...
+    -p                      Select port to host on (Default: 8888)
+    -v                      Verbose mode
+    -h                      Show this message
+    
+    Examples:
+    ./backend.sh -v -p 80   Host a server on port 80 and use verbose mode
+    ./backend.sh -v         Host a server on port 8888"
+}
+
 # Parse flags
 verbose=false;
-while getopts "vp:" option; do
+while getopts "hvp:" option; do
     case $option in
-        p)
-            port=$OPTARG;
-            shift
+        h)
+            usage
             ;;
         v)
             verbose=true;
+            ;;
+        p)
+            port=$OPTARG;
+            shift
             ;;
         *)
             echo "Bad or misplaced argument";
@@ -27,11 +42,11 @@ if [ -r 'frontend/index.html' ]; then
         if [ $verbose ]; then
             echo DEBUG: verbose
             # If the client entered the -v flag, print the output and save it to the log file
-            echo $response | nc -lvq 1 -p "${port:-8888}" 2>&1 | tee -a "logs/server.log"
+            echo "'$response'" | nc -lvq 1 -p "${port:-8888}" 2>&1 | tee -a "logs/server.log"
         else
-            echo not verbose
+            echo DEBUG: not verbose
             # If the client didn't enter the -v flag, save the output without printing it and notify about the connection 
-            echo $response | nc -lvq 1 -p "${port:-8888}" >> "logs/server.log" 
+            echo "'$response'" | nc -lvq 1 -p "${port:-8888}" >> "logs/server.log" 
             echo "Connection received."
         fi;
     done;
